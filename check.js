9de8adc3c43cdf92845eab719d3a4b9c3f1f67b2
Selina Zhang <yz4184@nyu.edu>
2019-10-04 16:36:08 -0400

    all change

--- check.js
+++ check.js
@@ -1,26 +1,28 @@
-function check(){
-	var ans1 = document.getElementById("input1");
-	var ans2 = document.getElementById("input2");
-	var ans3 = document.getElementById("input3");
-	var ans4 = document.getElementById("input4");
-	var ans5 = document.getElementById("input5");
-	var ans6 = document.getElementById("input6");
-	var ans7 = document.getElementById("input7");
-	var ans8 = document.getElementById("input8");
-	var ans9 = document.getElementById("input9");
-	var ans10 = document.getElementById("input10");
-	var ans11= document.getElementById("input11");
-	var ans12= document.getElementById("input12");
-	var ans13= document.getElementById("input13");
-	var ans14=document.getElementById("input14");
-	var ans15= document.getElementById("input15");
-	var ans16= document.getElementById("input16");
-	var ans17= document.getElementById("input17");
-	var ans18= document.getElementById("input18");
-	var ans19= document.getElementById("input19");
-	var ans20= document.getElementById("input20");
 
-	if (ans1 == "kogane" && ans2 == "blue" && ans3 == "tonic" && an4 == "budapest" && ans5 == "shunde" && ans6 == "keywest" && ans7=="coldspring" && ans8 == "35" && ans9 == "pinecone" && ans10 == "clark" && ans11 == "cold" && ans12 == "28" && ans13 == "cesuo" &&ans14 == "hudsonriver" && ans15 == "teacher" && ans16 == "jianimadamowang" && ans17 == "twodicks" && ans18 == "vodka" && ans19 == "masanbo" && ans20 == "milktea"){
+
+function check(){
+	var ans1 = document.getElementById("Input1").value;
+	var ans2 = document.getElementById("Input2").value;
+	var ans3 = document.getElementById("Input3").value;
+	var ans4 = document.getElementById("Input4").value;
+	var ans5 = document.getElementById("Input5").value;
+	var ans6 = document.getElementById("Input6").value;
+	var ans7 = document.getElementById("Input7").value;
+	var ans8 = document.getElementById("Input8").value;
+	var ans9 = document.getElementById("Input9").value;
+	var ans10 = document.getElementById("Input10").value;
+	var ans11= document.getElementById("Input11").value;
+	var ans12= document.getElementById("Input12").value;
+	var ans13= document.getElementById("Input13").value;
+	var ans14=document.getElementById("Input14").value;
+	var ans15= document.getElementById("Input15").value;
+	var ans16= document.getElementById("Input16").value;
+	var ans17= document.getElementById("Input17").value;
+	var ans18= document.getElementById("Input18").value;
+	var ans19= document.getElementById("Input19").value;
+	var ans20= document.getElementById("Input20").value;
+
+	if (ans1 == "kogane" && ans2 == "blue" && ans3 == "tonic" && ans4 == "budapest" && ans5 == "shunde" && ans6 == "keywest" && ans7=="coldspring" && ans8 == "35" && ans9 == "pinecone" && ans10 == "clark" && ans11 == "cold" && ans12 == "28" && ans13 == "cesuo" &&ans14 == "hudsonriver" && ans15 == "teacher" && ans16 == "jianimadamowang" && ans17 == "twodicks" && ans18 == "vodka" && ans19 == "masanbo" && ans20 == "milktea"){
 		location.replace("success.html")
 	}
 	else{
