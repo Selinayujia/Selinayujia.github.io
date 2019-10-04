5a9eaa061e1859937d9728332c8e9e97e47bc2af
Selina Zhang <yz4184@nyu.edu>
2019-10-04 16:11:56 -0400

    efforts

---
+++ check.js
@@ -0,0 +1,34 @@
+function check(){
+	var ans1 = document.getElementById("input1");
+	var ans2 = document.getElementById("input2");
+	var ans3 = document.getElementById("input3");
+	var ans4 = document.getElementById("input4");
+	var ans5 = document.getElementById("input5");
+	var ans6 = document.getElementById("input6");
+	var ans7 = document.getElementById("input7");
+	var ans8 = document.getElementById("input8");
+	var ans9 = document.getElementById("input9");
+	var ans10 = document.getElementById("input10");
+	var ans11= document.getElementById("input11");
+	var ans12= document.getElementById("input12");
+	var ans13= document.getElementById("input13");
+	var ans14=document.getElementById("input14");
+	var ans15= document.getElementById("input15");
+	var ans16= document.getElementById("input16");
+	var ans17= document.getElementById("input17");
+	var ans18= document.getElementById("input18");
+	var ans19= document.getElementById("input19");
+	var ans20= document.getElementById("input20");
+
+	if (ans1 == "kogane" && ans2 == "blue" && ans3 == "tonic" && an4 == "budapest" && ans5 == "shunde" && ans6 == "keywest" && ans7=="coldspring" && ans8 == "35" && ans9 == "pinecone" && ans10 == "clark" && ans11 == "cold" && ans12 == "28" && ans13 == "cesuo" &&ans14 == "hudsonriver" && ans15 == "teacher" && ans16 == "jianimadamowang" && ans17 == "twodicks" && ans18 == "vodka" && ans19 == "masanbo" && ans20 == "milktea"){
+		location.replace("success.html")
+	}
+	else{
+		location.replace("fail.html")
+
+	}
+
+
+
+
+}
